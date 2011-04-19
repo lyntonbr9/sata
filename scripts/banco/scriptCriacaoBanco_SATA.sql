f2f01aac6e5d96c2e3eb3757a25c034872d73878
lyntonbr <lyntonbr@gmail.com>
2011-04-19 17:14:36 -0300

    correcoes no script e adicao da tabela ativo

--- scripts/banco/scriptCriacaoBanco_SATA.sql
+++ scripts/banco/scriptCriacaoBanco_SATA.sql
@@ -1,21 +1,41 @@
+-- Table: "Ativo"
+
+-- DROP TABLE "Ativo";
+
+CREATE TABLE "Ativo"
+(
+  "codigoAtivo" character varying(10) NOT NULL,
+  "nomeEmpresa" character varying(500),
+  CONSTRAINT "Ativo_pkey" PRIMARY KEY ("codigoAtivo")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE "Ativo" OWNER TO postgres;
+
+
 -- Table: "CotacaoAtivo"
 
 -- DROP TABLE "CotacaoAtivo";
 
 CREATE TABLE "CotacaoAtivo"
 (
-  codigoativo character varying(10) NOT NULL,
-  periodo timestamp without time zone NOT NULL,
-  tipoperiodo character varying(4),
-  abertura character varying(15),
-  maxima character varying(15),
-  minima character varying(15),
-  fechamento character varying(15),
-  ano character varying(4),
-  CONSTRAINT "CotacaoAtivo_pkey" PRIMARY KEY (codigoativo, periodo)
+  "codigoAtivo" character varying(10) NOT NULL,
+  "periodo" timestamp without time zone NOT NULL,
+  "tipoperiodo" character varying(4),
+  "abertura" character varying(10),
+  "maxima" character varying(10),
+  "minima" character varying(10),
+  "fechamento" character varying(10),
+  "ano" character varying(4),
+  CONSTRAINT "CotacaoAtivo_pkey" PRIMARY KEY ("codigoAtivo", "periodo"),
+  CONSTRAINT "CotacaoAtivo_codigoativo_fkey" FOREIGN KEY ("codigoAtivo")
+      REFERENCES "Ativo" ("codigoAtivo") MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
 )
 WITH (
   OIDS=FALSE
 );
 ALTER TABLE "CotacaoAtivo" OWNER TO postgres;
+
 
