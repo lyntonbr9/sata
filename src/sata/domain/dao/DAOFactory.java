2771ad2e21b9edf94d7356822f4284346e6b3a3d
leojardim <leojardim@localhost>
2012-02-24 23:10:45 -0200

    Implementacao para funcionar no servidor externo (MochaHost)

--- src/sata/domain/dao/DAOFactory.java
+++ src/sata/domain/dao/DAOFactory.java
@@ -1,12 +1,14 @@
 package sata.domain.dao;
 
 //import sata.domain.dao.arquivo.ArquivoDAOFactory;
+import sata.domain.dao.mysql.MySQLDAOFactory;
 import sata.domain.dao.postgre.PostgreDAOFactory;
 
 public abstract class DAOFactory {
 	
 	public static final int ARQUIVO = 1;
 	public static final int POSTGRESQL = 2;
+	public static final int MYSQL = 3;
 	
 	public abstract IAtivoDAO getAtivoDAO();
 	public abstract ICotacaoAtivoDAO getCotacaoAtivoDAO();
@@ -18,6 +20,8 @@ public abstract class DAOFactory {
 //				return new ArquivoDAOFactory();
 			case POSTGRESQL:
 				return new PostgreDAOFactory();
+			case MYSQL:
+				return new MySQLDAOFactory();
 			default:
 				return null;
 		}
